03e00800043164cb2e23d0d6e4a891d2761b7415
marton552 <kugs4e@inf.elte.hu>
2022-04-20 19:05:18 +0200

    fixes

--- docs/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.intergalactic_fighters"},{"l":"com.intergalactic_fighters.backgrounds"},{"l":"com.intergalactic_fighters.sprites"},{"l":"com.intergalactic_fighters.sprites.enemies"}]
