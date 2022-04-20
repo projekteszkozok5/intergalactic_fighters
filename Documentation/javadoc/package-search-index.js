978f69ce0a4da68f2799135ea50924596a5bb513
marton552 <kugs4e@inf.elte.hu>
2022-04-20 18:02:51 +0200

    Documentation folder created

---
+++ Documentation/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.intergalactic_fighters"},{"l":"com.intergalactic_fighters.backgrounds"},{"l":"com.intergalactic_fighters.sprites"},{"l":"com.intergalactic_fighters.sprites.enemies"}]
